0e8450aa4e16944e80cc25ee11e5f6b34d845a27
zac <rodrigues.contano@gmail.com>
2023-06-02 09:52:30 -0300

    create header and input to add new task

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,26 @@
-import { Text, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+
 import { styles } from './styles'
+import { gray300 } from '../../tokens/colors'
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <View style={styles.header}>
+        <Text style={{ color: '#fff' }}>LOGOTIPO</Text>
+      </View>
+
+      <View style={styles.newTask}>
+        <TextInput
+          placeholder="Adicione uma nova tarefa"
+          placeholderTextColor={gray300}
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,11 +1,37 @@
 import { StyleSheet } from 'react-native'
 
-import { gray600 } from '../../tokens/colors'
+import { blueDark, gray500, gray600, gray700 } from '../../tokens/colors'
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: gray600,
-    padding: 30,
+  },
+  header: {
+    backgroundColor: gray700,
+    height: 173,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  newTask: {
+    flexDirection: 'row',
+    gap: 4,
+    padding: 20,
+    marginTop: -52,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    backgroundColor: gray500,
+    borderRadius: 8,
+    padding: 16,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 18,
+    backgroundColor: blueDark,
+    borderRadius: 8,
+    width: 52,
   },
 })
